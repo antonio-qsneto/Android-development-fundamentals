877fa77eee4023f9f714d087dec372b1b67b681b
antonio-qsneto <prof.antonioqsneto@gmail.com>
2021-04-16 19:13:18 -0300

    comment the log statement

--- app/src/main/java/com/antonio/getstarted/MainActivity.java
+++ app/src/main/java/com/antonio/getstarted/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Create a log mensage
         Log.d("MainActivity", "Hello World");
 
     }
